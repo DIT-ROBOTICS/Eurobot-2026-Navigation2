d1cd6f0f788a1aaaf0d97bcd5bc23e0d7d38fa4a
JustinShih <justinshih@gapp.nthu.edu.tw>
2026-03-20 12:50:37 +0800

    implement callback group and executor for goal reached subscription

--- src/Navigation2/nav2_behavior_tree/plugins/decorator/reset_recovery.cpp
+++ src/Navigation2/nav2_behavior_tree/plugins/decorator/reset_recovery.cpp
@@ -29,12 +29,21 @@ ResetRecovery::ResetRecovery(
   // Get the ROS node from the blackboard
   node_ = config().blackboard->get<rclcpp::Node::SharedPtr>("node");
 
+  // Setup callback group and executor for independent processing
+  callback_group_ = node_->create_callback_group(
+    rclcpp::CallbackGroupType::MutuallyExclusive,
+    false);
+  callback_group_executor_.add_callback_group(callback_group_, node_->get_node_base_interface());
+
   // Create subscription to goal_reached topic
   if (node_) {
+    rclcpp::SubscriptionOptions sub_options;
+    sub_options.callback_group = callback_group_;
     goal_reached_sub_ = node_->create_subscription<std_msgs::msg::Bool>(
       "goal_reached",
       10,
-      std::bind(&ResetRecovery::goalReachedCallback, this, std::placeholders::_1));
+      std::bind(&ResetRecovery::goalReachedCallback, this, std::placeholders::_1),
+      sub_options);
 
     RCLCPP_INFO(node_->get_logger(), "ResetRecovery: Subscribed to /goal_reached topic");
   } else {
@@ -47,6 +56,9 @@ ResetRecovery::ResetRecovery(
 
 BT::NodeStatus ResetRecovery::tick()
 {
+  // Process any pending callbacks in our executor
+  callback_group_executor_.spin_some();
+
   // Reset current_child_idx if goal was reached (thread-safe)
   if (goal_reached_) {
     setOutput("current_child_idx", 0);
